e94c24c20108a1c8feae643a450fa6e28a37e021
riyan1337 <73819984+riyan1337@users.noreply.github.com>
2020-11-02 15:52:02 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,87 @@
+<html>
+
+<head>
+
+	<title>Hacked by Riyan</title><link rel="shortcut icon" href="https://i.ibb.co/1LCyWRW/Creepypasta.jpg" "="" type="image/jpg">
+
+<meta property="og:image" content="https://i.ibb.co/1LCyWRW/Creepypasta.jpg" "="">
+
+<meta name="theme-color" content="black">
+
+<meta name="description" content="{ Infinity Cyber Team }">
+
+<meta name="description" color="black">
+
+<link href="https://fonts.googleapis.com/css?family=Kelly+Slab" rel="stylesheet" type="text/css">
+
+<style> html,
+
+ body { color: #FFFFFF; font-family: 'Courier', sans-serif; font-weight: 60; height: 60vh; margin: 0; }
+
+.full-height {
+
+	height: 67vh;
+
+}
+
+.flex-center {
+
+	align-items: center;
+
+	display: flex;
+
+	justify-content: center;
+
+}
+
+.position-ref {
+
+	position: relative;
+
+}
+
+.content {
+
+	text-align: center;
+
+}
+
+.title{
+
+	font-size: 36px;
+
+	padding: 20px;
+
+}
+
+a {
+
+	background: transparent;
+
+	color: white;
+
+	border: 1px solid black;
+
+}
+
+a:hover{
+
+	color: red;
+
+}
+
+</style> 
+
+</head>
+
+<body align="center" oncontextmenu="return false" bgcolor="black"><div class="stars"><div class="twinkling"><div class="flex-center position-ref full-height"> <div class="content"> <div class="text"><font color="white"> <img src="https://k.top4top.io/p_17678viwb0.gif" width="400" height="200"><br><font color="white"size="6">$ Hacked by Riyan $<br>
+
+<font color="red" size="4"><br><i>i wanna play, give me a toy :(</i></font><br><br>
+
+<font size="4">Greetz</font><font size="3"><br><br>LucySatan666 - Mr Onion - Mr.IX - Benz1337 - Indopride <br> Sedboi - ./Mr.xWanz403x - BabyMoon - XON 07<br><br><br><br>
+
+	&copy Infinity Cyber Team 2020
+
+<br><br><br><center><audio autoplay="autoplay" controls="controls" src="https://h.top4top.io/m_1692slz1d0.mp3" type="audio/mpeg"></audio> 
+
+</body></html>
